9c174d4ed60127de993deb0d2ae925f941b83cdd
qaate47 <antoine.willerval@the-qa-company.com>
2022-07-23 20:22:38 +0200

    reset warnings

--- scripts/package.ps1
+++ scripts/package.ps1
@@ -6,12 +6,12 @@ try {
     $base = (Get-Item $PSScriptRoot).parent
     Set-Location ($base.Fullname)
 
-    $version = npm run --silent version 2> $null
+    $version = npm run --silent version
     
-    Remove-Item -Recurse -Force CodApiRead -ErrorAction Ignore > $null
-    Copy-Item -Recurse dist CodApiRead > $null
+    Remove-Item -Recurse -Force CodApiRead -ErrorAction Ignore
+    Copy-Item -Recurse dist CodApiRead
 
-    Compress-Archive -Force -LiteralPath ".\CodApiRead\" -DestinationPath "CodApiRead.zip" > $null
+    Compress-Archive -Force -LiteralPath ".\CodApiRead\" -DestinationPath "CodApiRead.zip"
 
     Write-Output "::set-output name=version::$version"
 }
